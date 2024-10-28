9d88ed0001d2f4ef08d199fd2f413c7ba7a471ef
Soct29 <ledinhson291003@gmail.com>
2024-10-28 16:59:21 +0700

    controller

---
+++ be/controllers/postController.js
@@ -0,0 +1,267 @@
+import User from "../models/userModel.js";
+import Post from "../models/postModel.js";
+import { v2 as cloudinary } from "cloudinary";
+
+//create post
+const createPost = async (req, res) => {
+  try {
+    const { postedBy, text } = req.body;
+    let { img } = req.body;
+    if (!postedBy || !text) {
+      return res
+        .status(400)
+        .json({ error: "Posted and text fields are requieded" });
+    }
+
+    const user = await User.findById(postedBy);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (user._id.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ error: "unauthorized to create post!" });
+    }
+
+    const maxLength = 500;
+    if (text.length > maxLength) {
+      return res
+        .status(400)
+        .json({ error: `Text must be less than ${maxLength} characters` });
+    }
+
+    if (img) {
+      const uploadedRespone = await cloudinary.uploader.upload(img);
+      img = uploadedRespone.secure_url;
+    }
+
+    const newPost = new Post({ postedBy, text, img });
+
+    await newPost.save();
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log(err);
+  }
+};
+
+//get post
+const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found!" });
+    }
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log(err);
+  }
+};
+//delete Post
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    if (post.postedBy.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ error: "Unauthorized to delete post" });
+    }
+    if (post.img) {
+      const imgId = post.img.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imgId);
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Post deleted successfully!" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log(err);
+  }
+};
+
+//like and unlike post
+const likeUnlikePost = async (req, res) => {
+  try {
+    const { id: postId } = req.params;
+    const userId = req.user._id;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    const userLikedPost = post.likes.includes(userId);
+    if (userLikedPost) {
+      //unlike post
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      res.status(200).json({ message: "Post unliked successfully" });
+    } else {
+      //like post
+      post.likes.push(userId);
+      await post.save();
+      res.status(200).json({ message: "Post liked successfully!" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log(err);
+  }
+};
+
+//reply post
+
+const replyToPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
+    const userProfilePicture = req.user.profilePicture;
+    const username = req.user.username;
+
+    if (!text) {
+      return res.status(400).json({ error: "Text field is required" });
+    }
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const reply = { userId, text, userProfilePicture, username };
+    post.replies.push(reply);
+    await post.save();
+
+    res.status(200).json(reply);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log(err);
+  }
+};
+// delete replypost 
+const deleteReplyPost = async(req, res) => {
+  try {
+    const postId = req.params.id;
+    const replyId = req.body;
+    const post = await Post.findById(postId);
+    if(!post){
+      return  res.status(400).json({error: "Post not found!"})
+    }
+    const reply = post.replies.findIndex( reply => reply._id.toString() === replyId)
+    if(reply === -1){
+      return res.status(404).json({error: "Reply not found!"})
+    }
+    const deleteReply = post.replies.splice(reply,1);
+    await post.save();
+    res.status(200).json(deleteReply);
+
+  } catch (error) {
+    res.status(500).json({error: error.message})
+  }
+}
+
+//get feed post
+const getFeedPosts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+    const following = user.followings;
+
+    const feedPosts = await Post.find({ postedBy: { $in: following } }).sort({
+      createdAt: -1,
+    });
+
+    res.status(200).json(feedPosts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log(err);
+  }
+};
+
+//get user post
+const getUserPosts = async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const posts = await Post.find({ postedBy: user._id }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ error: message });
+  }
+};
+// update user post
+const updateUserPost = async (req, res) => {
+  const { text, img } = req.body;
+  const postId = req.params.id;
+
+  if (!req.user) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  try {
+    let post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    if (post.postedBy.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ error: "Unauthorized to update this post" });
+    }
+
+    if (!text && !img) {
+      return res.status(400).json({
+        error: "At least one of text or image must be provided for the update",
+      });
+    }
+
+    const maxLength = 500;
+    if (text && text.length > maxLength) {
+      return res
+        .status(400)
+        .json({ error: `Text must be less than ${maxLength} characters` });
+    }
+
+    if (img) {
+      if (post.img) {
+        const oldImageId = post.img.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(oldImageId);
+      }
+
+      const uploadedResponse = await cloudinary.uploader.upload(img);
+      post.img = uploadedResponse.secure_url;
+    }
+    if (text) {
+      post.text = text;
+    }
+    post = await post.save();
+    res.status(200).json({ message: "Post updated successfully!", post });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.error("Error updating post:", error);
+  }
+};
+
+export {
+  createPost,
+  getPost,
+  deletePost,
+  likeUnlikePost,
+  replyToPost,
+  getFeedPosts,
+  getUserPosts,
+  updateUserPost,
+  deleteReplyPost,
+};
