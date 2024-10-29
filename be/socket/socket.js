623f9e11c681ac19881148ee072d9577a43edbd3
quanda3tq <quanda3.tq@gmail.com>
2024-10-29 17:02:55 +0700

    update socket

--- be/socket/socket.js
+++ be/socket/socket.js
@@ -2,6 +2,7 @@ import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 
+//Socket
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
